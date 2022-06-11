2e0f79381880578159758aabc5ef53c6622c150b
Husain-Jinia <husainjinia0201@gmail.com>
2022-06-11 12:48:04 +0530

    Added profile & changed logic for registeration

--- user/views.py
+++ user/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from user.forms import UserRegistrationForm
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import  UpdateView
+from .models import *
+from .forms import *
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm
 
 # Create your views here.
-class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
-    template_name = 'register.html'
-    success_url= reverse_lazy('login')
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterProxy(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            print(form.cleaned_data)
+            print(str(form.cleaned_data))
+            print("here")
+    else:
+        form = UserRegisterProxy()
+    return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form  = ProfileUpdateForm(request.POST,request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'your account has been updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form  = ProfileUpdateForm(instance=request.user.profile)
+
+
+    context ={
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
