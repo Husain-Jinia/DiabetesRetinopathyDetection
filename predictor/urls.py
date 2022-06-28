ee96de88d57de1c8e7f819551170cbdd17cd4e21
Husain-Jinia <husainjinia0201@gmail.com>
2022-06-28 18:43:54 +0530

    Added logic and urls for heart disease

--- predictor/urls.py
+++ predictor/urls.py
@@ -2,6 +2,7 @@ import imp
 from django.contrib import admin
 from django.urls import path, include
 from predictor.Views.Disease.Diabetes import *
+from predictor.Views.Disease.HeartDisease import *
 from predictor.Views.Home import home
 
 
@@ -10,5 +11,8 @@ urlpatterns = [
     path('diabetesbasic/', diabetesbasic, name='diabetesbasic'),
     path('diabetesbasicresult/<int:pk>', diabetesbasicpred, name='diabetesbasicpred'),
     path('basicresult_pdf/<int:pk>', result_diabetes_basic_pdf, name='diabetesbasic_pdf'),
+    path('heartdisease/', heartdisease, name='heartdisease' ),
+    path('heartdiseaseresult/<int:pk>', heartdiseasepred, name="heartdiseaseresult"),
+    path('heartdisease_pdf/', result_heartdisease_pdf, name='heartdisease_pdf' ),
 ]
 
